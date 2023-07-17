7c6028a3ce65ccf339f386bd2261ab3f946808d8
Sheinart <sheinart09@gmail.com>
2023-07-17 17:24:08 +0300

    initial commit

---
+++ src/main/java/ru/netology/ManagerAfisha/ManagerAfisha.java
@@ -0,0 +1,54 @@
+package ru.netology.ManagerAfisha;
+
+public class ManagerAfisha {
+    private String[] films = new String[0];
+    private int limit;
+
+    public void setLimitFilms(int limit) {
+
+        this.limit = limit;
+    }
+
+    public int getLimitFilms() {
+
+        return limit;
+    }
+
+    public ManagerAfisha() {
+
+        this.limit = 10;
+    }
+
+    public ManagerAfisha(int limit) {
+
+        this.limit = limit;
+    }
+
+    public void addFilm(String film) {
+        String[] tmp = new String[films.length + 1];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length - 1] = film;
+        films = tmp;
+    }
+
+    public String[] findAll() {
+
+        return films;
+    }
+
+    public String[] findlast() {
+        int resultLength;
+        if (films.length < limit) {
+            resultLength = films.length;
+        } else {
+            resultLength = limit;
+        }
+        String[] tmp = new String[resultLength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = films[films.length - 1 - i];
+        }
+        return tmp;
+    }
+}
